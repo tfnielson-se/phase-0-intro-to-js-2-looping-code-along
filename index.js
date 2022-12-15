741fd7ede4599dbff9e627c2a545ca90fdb58ad0
Thomas <tfnielson.se@gmail.com>
2022-12-15 10:33:32 -0700

    commit tree 4d7f1d6f476554cba5420479db16452a488ae602

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = [`Guadalupe`, `Ollie`, `Aki`];
+const events = `surprise`;
+
+function writeCards(names, events) {
+    const newNames = []
+    for (let i = 0; i < names.length; i++){
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return newNames
+}
+writeCards(names, events);
+
+///////////////////////////////////////////////////////////
+
+const counter = 10
+
+function countDown (counter) {
+    while (counter > 0) {
+        console.log(counter)
+        counter -= 1;
+    }
+    console.log(counter);
+}
